160955bfc17b6a7b1840776a36e7b7606ec9cbb0
Flaneurette <30347642+flaneurette@users.noreply.github.com>
2026-01-09 22:38:11 +0100

    Create diskspace.sh

---
+++ Scripts/diskspace.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+THRESHOLD=80
+df -H | grep -vE '^Filesystem|tmpfs|cdrom' | awk '{ print $5 " " $1 }' | while read output;
+do
+  usage=$(echo $output | awk '{ print $1}' | cut -d'%' -f1)
+  partition=$(echo $output | awk '{ print $2 }')
+  if [ $usage -ge $THRESHOLD ]; then
+    echo "WARNING: Disk $partition is at ${usage}% capacity"
+  fi
+done
