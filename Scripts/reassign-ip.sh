835a8ef076f3681fc5ff563118fcec1febbfaa53
Alexandra <30347642+flaneurette@users.noreply.github.com>
2026-02-03 11:36:19 +0100

    Update: 2026-02-03 11:36

--- Scripts/reassign-ip.sh
+++ Scripts/reassign-ip.sh
@@ -1,11 +1,9 @@
-#!/bin/bash
-# sudo chmod +x reassign-ip.sh
 # Reassign a new master IP, revokes the old one.
 # This is useful, in case your(dynamic) IP changes
 # To find the chain name: sudo iptables -L -n -v --line-numbers
 # To find IP: sudo iptables -L -n -v --line-numbers | grep 100.2.3.4
 # Notice: this script removes UFW.
-# Notice: line prevents script from running suddenly. You MUST manually remove/comment this line:
+# Notice: line prevents script from running suddenly. You MUST manually remove/uncomment this line:
 exit 1
 
 # Root check.
@@ -17,15 +15,27 @@ fi
 # Edit these before running.
 
 # To find the chain name: sudo iptables -L -n -v --line-numbers
-CHAIN="ufw-user-input" # important, must match!
-# Old IP to revoke access
-OLD_IP="100.2.3.4"
+CHAIN="INPUT" # important, must match!
+
+# Old private IP to revoke access
+OLD_IP="1.2.3.4"
 # New IP
-NEW_IP="101.2.6.7"
+NEW_IP="1.2.3.5"
+
+# Server IP:
+SERVER_IP="5.4.3.2.1"
 
 # Ports the NEW IP is allowed to connect to:
 PORTS_TCP=(22 80 443 143 465 587 993 995 8080 8443)
 PORTS_UDP=(8080 8443)
+# Drop to WAN
+PORTS_TCPDROP=(22 143 465 587 993 995 8080 8443)
+
+# If you have tailscale:
+sudo iptables -A INPUT -i tailscale0 -p tcp --dport 22 -j ACCEPT
+
+# If you have mailserver:
+sudo iptables -A INPUT  -p tcp --dport 25 -j ACCEPT
 
 # Install netfilter-persistent (removes UFW!)
 # sudo apt update
@@ -39,7 +49,7 @@ for p in "${PORTS_UDP[@]}"; do
   sudo iptables -D $CHAIN -s $OLD_IP -p udp --dport $p -j ACCEPT 2>/dev/null
 done
 
-# Add new rules to INPUT chain. (recommended)
+# Add new IP rules to INPUT chain. (recommended)
 for p in "${PORTS_TCP[@]}"; do
   sudo iptables -A INPUT -s $NEW_IP -p tcp --dport $p -j ACCEPT
 done
@@ -47,5 +57,24 @@ for p in "${PORTS_UDP[@]}"; do
   sudo iptables -A INPUT -s $NEW_IP -p udp --dport $p -j ACCEPT
 done
 
+# Add new SERVER rules to INPUT chain. (recommended)
+for p in "${PORTS_TCP[@]}"; do
+  sudo iptables -A INPUT -s $SERVER_IP -p tcp --dport $p -j ACCEPT
+done
+for p in "${PORTS_UDP[@]}"; do
+  sudo iptables -A INPUT -s $SERVER_IP -p udp --dport $p -j ACCEPT
+done
+
+# drop for all others
+for p in "${PORTS_TCPDROP[@]}"; do
+  sudo iptables -A INPUT -p tcp --dport $p -j DROP
+done
+for p in "${PORTS_UDP[@]}"; do
+  sudo iptables -A INPUT -p udp --dport $p -j DROP
+done
+
 sudo netfilter-persistent save
+
+iptables-save > /etc/iptables/rules.v4
+
 echo "Firewall rules updated successfully."
