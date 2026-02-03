d231c1e2b97956f4751386b238c0f47aea735f00
Alexandra <30347642+flaneurette@users.noreply.github.com>
2026-02-03 14:58:24 +0100

    Update: 2026-02-03 14:58

--- Scripts/reassign-ip.sh
+++ Scripts/reassign-ip.sh
@@ -39,6 +39,10 @@ sudo iptables -A INPUT -i tailscale0 -p tcp --dport 22 -j ACCEPT
 # If you have a mailserver:
 sudo iptables -A INPUT  -p tcp --dport 25 -j ACCEPT
 
+# Allow web-access to all
+sudo iptables -A INPUT -p tcp --dport 80 -j ACCEPT
+sudo iptables -A INPUT -p tcp --dport 443  -j ACCEPT
+
 # Delete old rules
 for p in "${PORTS_TCP[@]}"; do
   sudo iptables -D $CHAIN -s $OLD_IP -p tcp --dport $p -j ACCEPT 2>/dev/null
