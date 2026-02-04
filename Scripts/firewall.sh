72c0fb9725a5ab530cc0c8544a03b72db4eaf4df
Alexandra <30347642+flaneurette@users.noreply.github.com>
2026-02-04 06:31:42 +0100

    Update: 2026-02-04 06:31

--- Scripts/firewall.sh
+++ Scripts/firewall.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Backup
+sudo iptables-save > /etc/iptables/rules.v4.bak.firewall
+sudo ip6tables-save > /etc/iptables/rules.v6.bak.firewall
+
+# Save
 sudo iptables-save > /etc/iptables/rules.v4
 sudo ip6tables-save > /etc/iptables/rules.v6
 
