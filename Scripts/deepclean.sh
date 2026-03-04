febf6d49ece3d7aa565627c78093100430d3cdaf
Alexandra <30347642+flaneurette@users.noreply.github.com>
2026-03-04 10:45:28 +0100

    Update: 2026-03-04 10:45

--- Scripts/deepclean.sh
+++ Scripts/deepclean.sh
@@ -10,12 +10,36 @@ echo "=== Cleaning package cache and autoremove ==="
 sudo apt clean
 sudo apt autoremove -y
 
+echo "=== Cleaning journal logs  ==="
+sudo journalctl --rotate
+sudo journalctl --vacuum-time=1s
+
 echo "=== Cleaning old .gz archives ==="
-sudo find /var/log -type f -name "*.gz" -mtime +60 -delete
+# sudo find /var/log -type f -name "*.gz" -mtime +60 -delete
+sudo find /var/log -name "*.gz" -delete
+sudo find /var/log -name "*.1" -delete
+sudo find /var/log -name "*.old" -delete
 
 echo "=== Cleaning tmp files  ==="
 sudo find /tmp -type f -atime +7 -delete
 sudo find /var/tmp -type f -atime +7 -delete
+
+sudo truncate -s 0  /var/log/lynis.log
+sudo truncate -s 0  /var/log/mail.log
+
+echo "=== Deep clean... ==="
+
+# Shred sensitive files.
+shred -u /var/log/auth.log
+touch /var/log/auth.log
+
+sudo truncate -s 0 /var/log/syslog
+sudo truncate -s 0 /var/log/kern.log
+sudo truncate -s 0 /var/log/dpkg.log         # Debian/Ubuntu
+sudo truncate -s 0 /var/log/apt/history.log
+# sudo truncate -s 0 /var/log/messages       # RHEL/CentOS
+# sudo truncate -s 0 /var/log/secure         # RHEL/CentOS
+sudo rm -rf /var/cache/logwatch/*
 
 echo "=== Disk usage after cleanup ==="
 df -h /
@@ -29,7 +53,7 @@ echo -e "${RED}    This prevents from sensitive data living in memory           
 echo -e "${RED}    To prevent this: use a LEADING SPACE before a sensitive command  ${NC}"
 echo -e "${RED}=====================================================================${NC}"
 
-# Shred it
+# Shred it first.
 shred -u ~/.bash_history
 touch ~/.bash_history
 
@@ -48,6 +72,11 @@ truncate -s 0 ~/wget-log
 for f in ~/.bash_history-*.tmp; do [ -f "$f" ] && > "$f"; done
 for f in ~/wget-log.*; do [ -f "$f" ] && > "$f"; done
 
+sudo systemctl stop auditd
+sleep 1
+sudo find /var/log/audit/ -type f -exec chmod 640 {} \;
+sudo find /var/log/audit/ -type f -exec truncate -s 0 {} \;
+sudo systemctl start auditd
 sleep 1
 sudo systemctl restart rsyslog
 # Finish
